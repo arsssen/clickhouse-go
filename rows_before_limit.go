dec95c9f47097ca5777b0989934a09c0fc195bcf
Arsen S <759223@gmail.com>
2021-09-07 18:20:52 +0400

    add check to RowsBeforeLimit not to panic when profileInfo is not set

--- rows_before_limit.go
+++ rows_before_limit.go
@@ -6,9 +6,13 @@ import (
 )
 
 //RowsBeforeLimit returns the  “rows_before_limit_at_least” (see https://clickhouse.tech/docs/en/interfaces/formats/#json)
+//should be called after retrieving results with rows.Next()
 func RowsBeforeLimit(r *sql.Rows) uint64 {
 	rowsIPtr := reflect.ValueOf(r).Elem().FieldByName("rowsi")
 	rowsI := reflect.Indirect(rowsIPtr).Elem()
 	profileInfoPtr := reflect.Indirect(rowsI).FieldByName("profileInfo")
+	if profileInfoPtr.IsNil() {
+		return 0
+	}
 	return reflect.Indirect(profileInfoPtr).FieldByName("rowsBeforeLimit").Uint()
 }
